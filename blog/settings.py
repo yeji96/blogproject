1423c8142a73bee3297df44aa2550b3798d4ba1c
yeji0601 <cyj902@naver.com>
2019-06-08 12:25:51 +0900

    second commit

--- blog/settings.py
+++ blog/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,3 +125,8 @@ STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'portfolio', 'static') ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
+
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
